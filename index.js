d5398787f8648b796bd162f66cf73b54d58b3660
Deepak <dgusaiwal537@gmail.com>
2022-11-30 16:51:54 +0530

    updating it to push in vercel becuase of cyclic it not working

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-const path = require('path');
-let express = require('express');
+let express = require("express");
 let app = express();
-let cors = require('cors')
-app.use(express.json())
+let cors = require("cors");
+app.use(express.json());
 app.use(cors());
-require('dotenv').config({path:".env"});
+require("dotenv").config({ path: ".env" });
 require("./Db/config");
 
 // Routes
-app.use('/user',require('./routes/userRotes'));
-app.use('/note',require('./routes/notesRoutes.js'));
+app.use("/user", require("./routes/userRotes"));
+app.use("/note", require("./routes/notesRoutes.js"));
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+// for the deployment
+// if ((process.env.NODE_ENV = "production")) {
+//   const path = require("path");
+//   app.use(express.static(path.join(__dirname, "./frontend/build")));
+//   app.get("*", function (_, res) {
+//     res.sendFile(
+//       path.join(__dirname, "./frontend/build/index.html"),
+//       function (err) {
+//         res.status(500).send(err);
+//       }
+//     );
+//   });
+// }
 
 
-// ======> just to understand how to .toSting() works (which we using in Upadte Note Function at userController)
-// const obj = new Object ("Deep")
-// console.log(obj.toString(),obj)
-const PORT = process.env.port || 5000
-app.listen(PORT)
+if(process.env.NODE_ENV=='production'){
+  const path = require('path')
+
+  app.get('/',(req,res)=>{
+      app.use(express.static(path.resolve(__dirname,'frontend','build')))
+      res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
